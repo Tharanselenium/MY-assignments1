6fc1da9f53b90d74032029713e97f0f26b795b07
Tharanselenium <tharanvikram.ofl@gmail.com>
2025-08-17 12:09:29 +0530

    Add files via upload
    day1

---
+++ day1/firefox.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class firefox {
+	public static void main(String[] args) {
+		float browsertype =100.2f;
+		String browsername= "firefox";
+		boolean isvisble= true;
+		int releaseyear =1998;
+		char browserlogo = 'f';
+		System.out.println("printing the data types of variables");
+		System.out.println("float:" + browsertype + "\n" +"string:" + browsername + "\n" +"boolean:" + isvisble + "\n" +"int:" + releaseyear + "\n" +"char:" + browserlogo);
+
+		
+	}
+
+}
