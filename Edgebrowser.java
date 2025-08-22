51bd808494df7ad577b3db5e70b4482a00c101f3
Tharanselenium <tharanvikram.ofl@gmail.com>
2025-08-22 19:17:27 +0530

    Add files via upload

---
+++ Edgebrowser.java
@@ -0,0 +1,11 @@
+package week1.day2;
+
+public class Edgebrowser {
+	public static void main(String[] args) {
+        browser browser = new browser();
+        browser.launchBrowser("Edge");
+        browser.closeBrowser("Edge");
+    }
+
+
+}
---
+++ primenumbers.java
@@ -0,0 +1,18 @@
+package week1.day2;
+
+
+
+public class primenumbers {
+	public static void main(String[] args) {
+		
+		for(int i=2;i<=15;i++) {
+		if(i%2==1) {
+			System.out.println(i+ "is a prime number");
+			
+		}
+		else {
+			System.out.println(i+"the number is not a prime number");
+		}
+	}}
+
+}
